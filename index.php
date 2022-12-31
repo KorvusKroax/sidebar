7ec5728e8e98cdf4544ff0622aa5f96ead8ee3d5
Korvus <korvuskroax@gmail.com>
2022-12-31 18:15:13 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta author="Korvus">
+
+    <link rel="stylesheet" href="style.css">
+
+    <title>sidebar</title>
+
+</head>
+
+<body>
+
+    <main>
+        <h1>Lorem ipsum</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce interdum, tellus non ultrices tempus, lorem metus finibus felis, et accumsan metus magna id dui. Sed dictum ac ante sit amet vehicula. Curabitur rutrum arcu nisi, ut condimentum turpis vulputate in. Nunc non gravida risus. Vestibulum sagittis dui vel nibh posuere scelerisque. Nam tellus tortor, consequat nec pellentesque nec, egestas vel nulla. Morbi porttitor volutpat mi, nec accumsan dui consectetur et. Nullam auctor porta feugiat. Nunc malesuada elit leo. Duis convallis libero a orci interdum, nec posuere enim condimentum. Duis sed dapibus leo. Pellentesque vitae arcu sed nisi venenatis malesuada. Quisque pharetra justo eu viverra placerat.</p>
+        <p>Curabitur ut quam volutpat, venenatis ex vitae, ultricies leo. Aenean porta tincidunt faucibus. Duis dignissim vitae augue sit amet imperdiet. Ut iaculis rutrum ligula congue malesuada. Nulla facilisi. Pellentesque et maximus tortor, non viverra purus. Maecenas at felis quam. Sed venenatis dapibus ex sagittis laoreet. Sed consectetur, metus eget consectetur suscipit, enim ex iaculis velit, at dictum nisi neque id mi. Praesent volutpat tempus turpis. Vivamus ullamcorper turpis at arcu venenatis, sit amet maximus nibh bibendum. Praesent in hendrerit erat, at luctus sem. Aliquam fringilla non elit nec faucibus. Duis gravida nibh ut tortor rhoncus, ac hendrerit metus fringilla.</p>
+        <p>Maecenas non risus vel lectus maximus commodo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In tristique justo eget erat blandit, a tincidunt nisl egestas. Vestibulum tempor non mi sit amet volutpat. Nunc faucibus magna nec dolor luctus dapibus. Fusce eget efficitur nunc. Nam pharetra nunc quis mi consectetur dapibus. Suspendisse et sodales mi. Nulla vitae porta ipsum. In a leo fermentum, tincidunt ante eu, tempor sem. Donec sollicitudin et ipsum vel imperdiet. Cras vel dictum diam. Sed eleifend facilisis purus. Nulla a lorem at nisl malesuada sollicitudin vel et neque.</p>
+        <p>Cras posuere ex sit amet quam rutrum eleifend. Mauris vulputate, lacus id eleifend condimentum, felis nisl aliquam tortor, nec tristique nulla urna vel leo. Nam aliquet vitae dolor non cursus. Sed fringilla aliquam lacus consectetur varius. Ut at augue eu libero pellentesque auctor. Integer augue sapien, imperdiet vitae tempor sed, volutpat varius mauris. Pellentesque leo ipsum, vulputate vel finibus sed, mattis placerat lacus. Pellentesque commodo felis et elit fringilla, tincidunt molestie turpis consequat. Aliquam erat volutpat. Praesent venenatis nibh arcu, in euismod turpis consectetur eget. Sed ullamcorper rhoncus tortor, et iaculis lorem hendrerit sit amet. Vivamus suscipit eleifend nibh sed commodo. Duis egestas pellentesque odio, pretium congue sapien placerat at. Quisque vel tincidunt lectus, pharetra auctor felis. Nullam metus lacus, viverra quis metus nec, gravida rhoncus arcu. Mauris placerat cursus mauris id pharetra.</p>
+        <p>Donec accumsan turpis non lacus viverra ullamcorper. Integer mollis, dui at ornare blandit, ipsum dolor ullamcorper erat, dapibus iaculis nisl leo pretium erat. Sed ante dui, ultricies at purus eget, consectetur pharetra magna. Pellentesque in convallis neque. Integer sit amet varius sem. Suspendisse interdum varius dui a blandit. Integer et mi eget purus venenatis pulvinar.</p>
+        <p>Nulla hendrerit commodo turpis, a scelerisque risus. In convallis, sem et ultricies auctor, ligula dolor suscipit sapien, eget rhoncus nisi tellus sed tortor. Aenean eu egestas lorem. Mauris malesuada leo tortor, vitae mollis eros elementum sed. Sed volutpat pharetra lectus id rutrum. Nulla risus enim, commodo id tellus id, scelerisque consequat metus. Vestibulum sit amet mi eu quam tincidunt vestibulum. Ut interdum, metus et sagittis fringilla, justo dui laoreet justo, in vestibulum neque nunc et ante. Nunc ultrices, tellus et luctus sagittis, turpis leo tempus odio, non tincidunt urna lorem nec enim. Quisque scelerisque sed velit sed facilisis. Curabitur eget consectetur mauris. Sed eu enim ac eros bibendum tempus eget id velit. Vivamus et erat vel turpis mattis consectetur. Fusce ultrices tortor nec justo ullamcorper posuere.</p>
+        <p>Duis cursus gravida molestie. Vivamus faucibus magna at feugiat gravida. Vivamus imperdiet id tellus non cursus. Aenean varius nisi lectus, in semper urna condimentum quis. Proin ut auctor ante, vestibulum lacinia risus. Morbi id gravida est, a varius eros. Nullam sed sodales nisi, a vehicula nulla. Ut viverra non dui at vestibulum. Fusce pulvinar sodales efficitur. Aliquam luctus orci sit amet maximus aliquet. Phasellus sit amet nisl elementum, vestibulum libero nec, tincidunt velit.</p>
+        <p>Phasellus vitae eros a orci dapibus pellentesque. Aliquam tristique eleifend diam ut eleifend. Maecenas in est nec nibh pharetra efficitur vel vitae est. Quisque lobortis ultrices ante vel condimentum. Cras gravida nisl ac ultricies euismod. Maecenas blandit dolor quis augue malesuada, et placerat risus convallis. Vestibulum varius mauris sit amet erat commodo, non imperdiet elit tempus.</p>
+        <p>Ut hendrerit aliquet libero vitae molestie. Etiam et ex vehicula, fermentum elit sed, efficitur libero. In a orci at nunc lobortis consequat. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec viverra magna nec ipsum pretium rhoncus. Suspendisse vehicula varius urna. Praesent lacinia nunc elit, et porttitor dolor malesuada quis. Vestibulum non pulvinar nisl. Nam elit neque, aliquam quis leo non, dictum tristique quam. Aenean faucibus porttitor turpis suscipit scelerisque.</p>
+        <p>Fusce sagittis sem tincidunt, eleifend justo et, bibendum sapien. Ut nisl dui, aliquet vel mauris sed, malesuada interdum arcu. Vestibulum vitae quam est. Quisque sodales placerat massa non egestas. Nunc vel quam ut sapien ullamcorper semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce tristique eros in felis tempor sodales. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+    </main>
+
+    <?php require("sidebar.php"); ?>
+
+</body>
+
+</html>
---
+++ sidebar.php
@@ -0,0 +1,17 @@
+<input id="input-checkbox" type="checkbox">
+<label for="input-checkbox">
+    <span class="sidebar-button bars">&#9776;</span>
+    <span class="sidebar-button times">&#10005;</span>
+</label>
+
+<div class="sidebar-backdrop"></div>
+<div class="sidebar">
+    <header>menu header</header>
+    <ul>
+        <li><a href="#">home</a></li>
+        <li><a href="#">articles</a></li>
+        <li><a href="#">post</a></li>
+        <li><a href="#">contact</a></li>
+    </ul>
+    <footer>menu footer</footer>
+</div> 
